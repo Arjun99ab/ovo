b75bde5617dce7b434c05ab33bedbb9213810054
Arjun99ab <53882059+Arjun99ab@users.noreply.github.com>
2022-10-08 10:26:36 -0400

    kaizo

--- src/index.js
+++ src/index.js
@@ -39,3 +39,10 @@ function reverseSelect() {
     document.getElementById("launchBtn").innerHTML = "Launch Reverse"
     document.getElementById("launchBtn").href = "./reverse/"
 }
+
+function kaizoSelect() {
+    document.getElementById("versionName").innerHTML = "Modded OvO Kaizo"
+    document.getElementById("versionDesc").innerHTML = "OvO Kaizo is a spinoff game created by Seblertyg#7678. This modloader includes all of the mods including multiplayer and others."
+    document.getElementById("launchBtn").innerHTML = "Launch Kaizo"
+    document.getElementById("launchBtn").href = "./kaizo/"
+}
